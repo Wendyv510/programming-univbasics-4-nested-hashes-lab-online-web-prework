5b7ebb3e1b2da706b58a95a60d1e0e9a90f7850a
Wendy Harris <wendyv510@hotmail.com>
2019-09-22 21:22:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,6 +66,9 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
+{:yukihiro_matsumoto=>{
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"]}}.unshift
 
 	programmer_hash = 
  		{
